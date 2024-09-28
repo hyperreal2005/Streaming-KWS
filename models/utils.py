0797728a27cabb019c7e876d63068f83c65888fb
Pranitdeep Singh <145380583+hyperreal2005@users.noreply.github.com>
2024-09-28 17:06:23 +0530

    Update utils.py

--- models/utils.py
+++ models/utils.py
@@ -28,7 +28,7 @@ from kws_streaming.models import model_params
 from kws_streaming.models import model_utils
 from kws_streaming.models import models as kws_models
 
-models_utils = tf._keras_internal.models  # pylint: disable=protected-access
+models_utils = tf1._keras_internal.models   # pylint: disable=protected-access
 functional = tf._keras_internal.engine.functional  # pylint: disable=protected-access
 
 
